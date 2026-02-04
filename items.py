3f690d4efaab2fba41f1eb67ad54d87a7e0044c7
Tawhaw <tahasaffih@cmail.carleton.ca>
2026-02-04 16:22:20 -0500

    Image attribution

---
+++ items.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+
+
+Player = Image.open("Character.png").convert("RGBA")
+Coin = Image.open("Coin.jpg").convert("RGBA")
+Vault = Image.open("Vault.png").convert("RGBA")
+Door = Image.open("Door.png").convert("RGBA")
